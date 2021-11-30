8cf6c0e751ba8fe4ba8d00619724939a218524db
Andy Guzman <andyguzman117@gmail.com>
2021-11-30 11:58:21 -0400

    implementacion de la ventana de informacion

---
+++ app/src/main/java/com/teamcode/creamy/Activity_About.java
@@ -0,0 +1,27 @@
+package com.teamcode.creamy;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.app.FragmentTransaction;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class Activity_About extends AppCompatActivity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+    }
+
+
+}
