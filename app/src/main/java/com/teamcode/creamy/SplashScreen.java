ef397cbab93c164dc533e356b0704ecd350d373c
Daniel <dansan1234567@gmail.com>
2021-11-30 22:12:48 -0400

    SPLASH

--- app/src/main/java/com/teamcode/creamy/SplashScreen.java
+++ app/src/main/java/com/teamcode/creamy/SplashScreen.java
@@ -2,7 +2,12 @@ package com.teamcode.creamy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -10,5 +15,18 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        getSupportActionBar().hide();
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+            }
+        };
+
+        Timer time = new Timer();
+        time.schedule(task, 3000);
     }
 }
