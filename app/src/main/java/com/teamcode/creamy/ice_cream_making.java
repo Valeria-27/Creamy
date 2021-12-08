90e5db29b2bea7d98a4edfa68c79ddd1dc94be58
Andy Guzman <andyguzman117@gmail.com>
2021-12-07 23:19:21 -0400

    implementacion de cambios del combobox

--- app/src/main/java/com/teamcode/creamy/ice_cream_making.java
+++ app/src/main/java/com/teamcode/creamy/ice_cream_making.java
@@ -4,9 +4,14 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -28,12 +33,13 @@ public class ice_cream_making extends AppCompatActivity {
         setContentView(R.layout.activity_ice_cream_making);
         comboContainer = findViewById(R.id.spinnerContainers);
         comboFirtsFlavour = findViewById(R.id.spinnerFirstFlavor);
+        comboSecondFlavour = findViewById(R.id.spinnerSecondFlavor);
+        comboThreeFlavour = findViewById(R.id.spinnerThreeFlavor);
 
         mDatabase = FirebaseDatabase.getInstance().getReference();
         loadContainers();
         loadFlavor();
-        loadSecondFlavour();
-        loadThreeFlavour();
+
     }
 
     public void loadContainers(){
@@ -57,7 +63,8 @@ public class ice_cream_making extends AppCompatActivity {
         });
     }
     public void loadFlavor(){
-        List<Flavor> flavors = new ArrayList<>();
+        ArrayList<Flavor> flavors = new ArrayList<>();
+
         mDatabase.child("flavors").addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
@@ -66,63 +73,27 @@ public class ice_cream_making extends AppCompatActivity {
                         String id = ds.getKey();
                         String name = ds.child("name").getValue().toString();
                         flavors.add(new Flavor(id,name));
+
                     }
-                    ArrayAdapter<Flavor> arrayAdapter = new ArrayAdapter<>(ice_cream_making.this, android.R.layout.simple_dropdown_item_1line, flavors);
+                    ArrayAdapter<Flavor> arrayAdapter = new ArrayAdapter<>(ice_cream_making.this, android.R.layout.simple_spinner_dropdown_item, flavors);
+
                     comboFirtsFlavour.setAdapter(arrayAdapter);
+                    comboSecondFlavour.setAdapter(arrayAdapter);
+                    comboThreeFlavour.setAdapter(arrayAdapter);
+
                 }
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-
+                // Getting Post failed, log a message
+                Log.w("ERROROROROROR", "loadPost:onCancelled", error.toException());
             }
         });
+
+
     }
 
-    public void loadSecondFlavour(){
-        List<Flavor> flavors = new ArrayList<>();
-        mDatabase.child("flavors").addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if(snapshot.exists()){
-                    for (DataSnapshot ds: snapshot.getChildren()){
-                        String id = ds.getKey();
-                        String name = ds.child("name").getValue().toString();
-                        flavors.add(new Flavor(id,name));
-                    }
-                    ArrayAdapter<Flavor> arrayAdapterTwo = new ArrayAdapter<>(ice_cream_making.this, android.R.layout.simple_dropdown_item_1line, flavors);
-                    comboSecondFlavour.setAdapter(arrayAdapterTwo);
-                }
-            }
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-    }
-    public void loadThreeFlavour(){
-        List<Flavor> flavors = new ArrayList<>();
-        mDatabase.child("flavors").addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if(snapshot.exists()){
-                    for (DataSnapshot ds: snapshot.getChildren()){
-                        String id = ds.getKey();
-                        String name = ds.child("name").getValue().toString();
-                        flavors.add(new Flavor(id,name));
-                    }
-
-                    ArrayAdapter<Flavor> arrayAdapterThree = new ArrayAdapter<>(ice_cream_making.this, android.R.layout.simple_dropdown_item_1line, flavors);
-                    comboThreeFlavour.setAdapter(arrayAdapterThree);
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-    }
 
 
 }
